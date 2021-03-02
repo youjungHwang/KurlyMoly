ed82bcfeb8c93bf0e3eb561926876e3331947cd1
nah2ee <dlth0815@gmail.com>
2021-03-02 16:34:36 +0900

    1st code

---
+++ script/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// $(document).ready(function () {
+//     $("#top-message-close").on("click", function () {
+//         setCookie('top_msg_banner2', 'set_cookie', 1)
+//     });
+//     if (getCookie('top_msg_banner2') == 'set_cookie') {
+//         $("#top-message").hide()
+//     } else {
+//         $("#top-message").show();
+//     }
+// });
+
+// function setCookie(cookieName, value, exdays) {
+//     var exdate = new Date();
+//     exdate.setDate(exdate.getDate() + exdays);
+//     var cookieValue = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toGMTString());
+//     document.cookie = cookieName + "=" + cookieValue + "; path=/;"
+// }
+
+// function getCookie(cookieName) {
+//     cookieName = cookieName + '=';
+//     var cookieData = document.cookie;
+//     var start = cookieData.indexOf(cookieName);
+//     var cookieValue = '';
+//     if (start != -1) {
+//         start += cookieName.length;
+//         var end = cookieData.indexOf(';', start);
+//         if (end == -1) end = cookieData.length;
+//         cookieValue = cookieData.substring(start, end);
+//     }
+//     return unescape(cookieValue);
+// }
+
