cbf155d5706912196a3155ce5127c87b881109bf
hyejung <power_leader@naver.com>
2021-03-22 02:20:56 +0900

    관리자페이지- admin_login.html 추가/ productadd , noticeadd css 변경/  side menu  <a href> 변경

--- script/script.js
+++ script/script.js
@@ -32,3 +32,19 @@
 //     return unescape(cookieValue);
 // }
 
+
+$('.cc_kakao .tit').on('click',function(e){
+    e.preventDefault();
+    $.ajax({
+        type: "GET",
+        url: apiDomain+'/v1/mypage/asks/confirm/kakao',
+        dataType: 'json',
+        success: function(data) {
+            if(data.data.ok_button_action_url.indexOf('https://api.happytalk.io/') > -1){
+                if(confirm('['+data.data.title+'] '+data.data.message)) window.open(data.data.ok_button_action_url,'_blank');
+            }else{
+                alert(data.data.title+'\n'+data.data.message);
+            }
+        }
+    })
+});
